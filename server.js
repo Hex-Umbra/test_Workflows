40d0d8cfdf6ec9efe31236dc5dab11421120b582
Hex-Umbra <g.p.dacosta@hotmail.com>
2025-04-10 09:50:27 +0200

    `First Commit`

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3333;
+
+app.get("/", (req,res)=>{
+    res.json("Hello World From gitAction")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
